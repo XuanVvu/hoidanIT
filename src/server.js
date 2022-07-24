d776cd5c33530b9154e5b7bf60cfcd80a90f2f03
XuanVu <nguyenxuanvu0307.tx@gmail.com>
2022-07-24 22:58:33 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+require('dotenv').config();
+
+
+
+let app = express();
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB();
+
+
+let port = process.env.PORT || 6060;
+// Port === undefined => port = 6060
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
